320bdcdd2b9d055839e57624c7893a6aca62a6ae
Yusuf Rizal <yusufrizalh@gmail.com>
2021-09-13 15:17:37 +0700

    update to part3

---
+++ app-part3.js
@@ -0,0 +1,44 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      url1: "http://inixindo.id",
+      url2: "http://inixindosurabaya.id",
+      books: [
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "./assets/image_1.jpg",
+          isFav: false,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "./assets/image_2.jpg",
+          isFav: false,
+        },
+        {
+          title: "Name of The Wind",
+          author: "Patrick Rothfuss",
+          img: "./assets/image_3.jpg",
+          isFav: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
+    },
+  },
+});
+
+app.mount("#app");
